53292ccd013c1f28a58c672e3eab0d3de4b61265
Tinagoldstein <73163411+Tinagoldstein@users.noreply.github.com>
2022-02-13 18:05:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(300, 250); 
+    video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw() {
+    image(video, 170, 100, 350, 300);
+  
+    fill(0, 200, 0);
+    stroke(0, 0, 255);
+    circle(50, 50, 80);
+    circle(50, 430, 80);
+    circle(590, 50, 80);
+    circle(590, 430, 80);
+  
+    fill(0, 0, 200);
+    stroke(50, 128, 0);
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(40, 90, 20, 300);
+    rect(580, 90, 20, 300);
+  }
+
+
